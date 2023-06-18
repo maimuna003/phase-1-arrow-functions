638aa1d2d39ecbb9db00559f6cc53786f390684b
maimuna <mohamudmaimuna003@gmail.com>
2023-06-18 20:04:06 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+// function expression that divides two numbers.
+function divide(a, b) {
+    return a / b;
+  }
+  
+  // square arrow function that multiplies a number by itself
+  let a = 2;
+  const square = (a) => {
+    return a * a;
+  };
+  square(a);
+  
+  // add arrow function that adds two numbers
+  const add = (a, b) => {
+    return a + b;
+  };
+  let c = 7;
+  let d = 0;
+  
+  add(7, 0);
